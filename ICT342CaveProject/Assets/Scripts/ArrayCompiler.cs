3c6cd258d3c24857b13d416a8e5fa0ad0c7e4401
somerandomawsomeguy <61770600+SomeRandomAwesomeGuy@users.noreply.github.com>
2022-04-17 22:41:07 +1000

    added Empty folders and scripts then coded file retrieval

---
+++ ICT342CaveProject/Assets/Scripts/ArrayCompiler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayCompiler : MonoBehaviour {
+
+	object[] List;
+	bool check = false;
+	int ArrayLength;
+	// Use this for initialization
+	void Start () {
+		//loads all objects in the folder and goes through each
+		foreach (object Display in Resources.LoadAll("Assets/Displays"))
+        {
+			//resets variables for each iteration of the loop
+			ArrayLength = List.Length;
+			check = false;
+
+			//check that the object is not already in the array
+			foreach (object Saved in List)
+            {
+				if (Display == Saved)
+                {
+					check = true;
+					break;
+                }
+            }
+
+			//if object is not in the array then add it
+			if (check == false)
+            {
+				List[ArrayLength++] = Display;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
