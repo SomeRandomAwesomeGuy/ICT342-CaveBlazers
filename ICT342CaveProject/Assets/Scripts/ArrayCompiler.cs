fa49a69350070325f55ab110921d3436fde26176
usc-DRO003 <37055844+usc-DRO003@users.noreply.github.com>
2022-05-17 22:49:24 +1000

    Video Commit Pt.1
    Adding in video player for cylinder object

--- ICT342CaveProject/Assets/Scripts/ArrayCompiler.cs
+++ ICT342CaveProject/Assets/Scripts/ArrayCompiler.cs
@@ -6,19 +6,23 @@ using System.Linq;
 public class ArrayCompiler : MonoBehaviour {
 
 	public List<Texture> DisplayList = new List<Texture>();
-	bool check = false;
+    public List<Texture> DisplayListVideo = new List<Texture>();
+    bool check = false;
 	int ListLength;
-	public Texture alt;
+    int VideoListLength;
+    public Texture alt;
 	Texture Display;
+    Texture VideoDisplay;
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log("start the compilation");
 
 		var loadedObjects = Resources.LoadAll("Displays", typeof(Texture));
+        var loadedObjectsVideo = Resources.LoadAll("VideoDisplay", typeof(Texture));
 
-		//loads all objects in the folder and goes through each
-		foreach (var DisplayToBe in loadedObjects)
+        //loads all objects in the folder and goes through each
+        foreach (var DisplayToBe in loadedObjects)
         {
 			Debug.Log("run loop");
 			Debug.Log("add" + DisplayToBe.name);
@@ -44,39 +48,92 @@ public class ArrayCompiler : MonoBehaviour {
         }
 
 		ListLength = DisplayList.Count;
-		//DisplayList.Add(alt);
-	}
-	
-	public Texture GetListPoint(int PointOfArray)
-    {
-		if(ListLength > 1)
-        {
-			Debug.Log("list greater than one");
-			while (PointOfArray > (ListLength-1))
-			{
-				PointOfArray -= ListLength;
-			}
+        //DisplayList.Add(alt);
 
-			while (PointOfArray < 0)
-			{
-				PointOfArray += ListLength;
-			}
-		}
-        else
+        foreach (var DisplayVideobe in loadedObjectsVideo)
         {
-			Debug.Log("list at one");
-			PointOfArray = 0;
+            Debug.Log("run loop");
+            Debug.Log("add" + DisplayVideobe.name);
+
+            //resets variables for each iteration of the loop
+            check = false;
+
+            //check that the object is not already in the array
+            /*foreach (Material Saved in DisplayList)
+            {
+				if (DisplayToBe == Saved)
+                {
+					check = true;
+					break;
+                }
+            }*/
+
+            //if object is not in the array then add it
+            if (check == false)
+            {
+                DisplayListVideo.Add(DisplayVideobe as Texture);
+            }
         }
 
-		Debug.Log("searching for image number" + PointOfArray.ToString());
+        VideoListLength = DisplayListVideo.Count;
+    }
 
-        Display = DisplayList[PointOfArray];
+    public Texture GetListPoint(int PointOfArray)
+    {
+        int totallength = ListLength + VideoListLength;
+        if (totallength > 1)
+        {
+            Debug.Log("list greater than one");
+            while (PointOfArray < 0)
+            {
+                PointOfArray += totallength;
 
-		if (Display == null)
-		{
-			Display = alt;
-		}
+                if (PointOfArray >= ListLength && PointOfArray < totallength)
+                {
+                    PointOfArray -= ListLength;
+                    Display = DisplayListVideo[PointOfArray];
+                }
+            }
 
-		return Display;
+            while (PointOfArray > (totallength - 1))
+            {
+                if (PointOfArray > totallength) 
+                {
+                    PointOfArray -= totallength;
+                }
+
+                else if (PointOfArray < ListLength)
+                {
+                    Display = DisplayList[PointOfArray];
+                }
+
+                else if (PointOfArray >= ListLength && PointOfArray < totallength)
+                {
+                    PointOfArray -= ListLength;
+                    Display = DisplayListVideo[PointOfArray];
+                }                       
+            }
+                        
+        }
+        else
+        {
+            Debug.Log("list at one");
+            PointOfArray = 0;
+        }
+
+        Debug.Log("searching for image number" + PointOfArray.ToString());
+
+        if (Display == null)
+        {
+            Display = alt;
+        }
+
+        return Display;
+    }
+
+    public Texture ListPointVideo(int VideoPointOfArray)
+    {
+        return DisplayListVideo[VideoPointOfArray];
+
     }
 }
